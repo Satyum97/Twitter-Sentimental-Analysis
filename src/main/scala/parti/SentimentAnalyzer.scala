8d739cc6ba587544d84dc5532134d71b73a6be17
Satyam Bhikadiya <35142308+Satyum97@users.noreply.github.com>
2021-05-15 11:32:23 -0500

    Add files via upload

---
+++ src/main/scala/parti/SentimentAnalyzer.scala
@@ -0,0 +1,40 @@
+package parti
+
+import java.util.Properties
+
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
+import parti.Sentiment._
+
+
+import scala.collection.convert.wrapAll._
+
+object SentimentAnalyzer {
+
+  val properties = new Properties()
+  properties.setProperty("annotators", "tokenize, ssplit, parse, sentiment")
+  val pipeline: StanfordCoreNLP = new StanfordCoreNLP(properties)
+
+  def mainSentiment(input: String): Sentiment = Option(input) match {
+    case Some(text) if !text.isEmpty => extractSentiment(text)
+    case _ => throw new IllegalArgumentException("Input can't be empty")
+  }
+
+  def extractSentiment(text: String): Sentiment = {
+    val (_, sentiment) = extractSentiments(text)
+      .maxBy { case (sentence, _) => sentence.length }
+    sentiment
+  }
+
+  def extractSentiments(text: String): List[(String, Sentiment)] = {
+    val annotation: Annotation = pipeline.process(text)
+    val sentences = annotation.get(classOf[CoreAnnotations.SentencesAnnotation])
+    sentences.map(sentence => (sentence, sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])))
+      .map { case (sentence, tree) => (sentence.toString, Sentiment.toSentiment(RNNCoreAnnotations.getPredictedClass(tree))) }
+      .toList
+
+  }
+}
+
